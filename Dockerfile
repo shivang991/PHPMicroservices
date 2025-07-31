da0d168d61089afb126142114113cf2c67cd3a7f
shivang991 <32.shivangdubey@gmail.com>
2025-07-31 05:34:40 +0000

    feat: add nginx and other docker changes

--- Dockerfile
+++ Dockerfile
@@ -1,43 +1,7 @@
-FROM node:20-slim AS frontend-build
-
-ENV PNPM_HOME="/pnpm"
-ENV PATH="$PNPM_HOME:$PATH"
-RUN corepack enable
-
-WORKDIR /app
 
 
-COPY frontend-web/ .
-RUN pnpm install
-RUN pnpm build
+FROM php:8.3-fpm-alpine
 
+ARG BACKEND_DIR
 
-# FROM php:8.3-apache as final
-
-# RUN docker-php-ext-install pdo pdo_mysql
-
-# COPY ./index.php /var/www/html/index.php
-
-# RUN mkdir /var/www/html/frontend-web
-# COPY --from=frontend-build /app/dist/ /var/www/html/frontend-web/dist/
-
-# RUN mv "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
-
-# # USER www-data
-# # CMD ["apache2-foreground"]
-
-# FROM nginx:alpine3.22-slim AS final
-# FROM nginx AS final
-
-# # RUN apk add --no-cache nginx-
-
-
-# COPY public /srv/phpmicroservices/public
-# COPY nginx.conf /etc/nginx/conf.d/default.conf
-
-# # FROM php:8.4-fpm
-
-FROM php:8.3-fpm AS final
-
-COPY --from=frontend-build /app/dist/ /srv/phpmicroservices/public/frontend-web/dist/
-COPY public /srv/phpmicroservices/public
+COPY public "$BACKEND_DIR/public"
--- public/index.php
+++ public/index.php
@@ -7,94 +7,14 @@ function dd(mixed $data)
     echo "</pre>";
 }
 
-function get_mime_type($filename)
-{
-    $exploded = explode(".", $filename);
-    $ext = strtolower($exploded[count($exploded) - 1]);
-
-    $mimetypes = [
-        "txt" => "text/plain",
-        "htm" => "text/html",
-        "html" => "text/html",
-        "php" => "text/html",
-        "css" => "text/css",
-        "js" => "application/javascript",
-        "json" => "application/json",
-        "xml" => "application/xml",
-        "swf" => "application/x-shockwave-flash",
-        "flv" => "video/x-flv",
-
-        // images
-        "png" => "image/png",
-        "jpe" => "image/jpeg",
-        "jpeg" => "image/jpeg",
-        "jpg" => "image/jpeg",
-        "gif" => "image/gif",
-        "bmp" => "image/bmp",
-        "ico" => "image/vnd.microsoft.icon",
-        "tiff" => "image/tiff",
-        "tif" => "image/tiff",
-        "svg" => "image/svg+xml",
-        "svgz" => "image/svg+xml",
-
-        // archives
-        "zip" => "application/zip",
-        "rar" => "application/x-rar-compressed",
-        "exe" => "application/x-msdownload",
-        "msi" => "application/x-msdownload",
-        "cab" => "application/vnd.ms-cab-compressed",
-
-        // audio/video
-        "mp3" => "audio/mpeg",
-        "qt" => "video/quicktime",
-        "mov" => "video/quicktime",
-
-        // adobe
-        "pdf" => "application/pdf",
-        "psd" => "image/vnd.adobe.photoshop",
-        "ai" => "application/postscript",
-        "eps" => "application/postscript",
-        "ps" => "application/postscript",
-
-        // ms office
-        "doc" => "application/msword",
-        "rtf" => "application/rtf",
-        "xls" => "application/vnd.ms-excel",
-        "ppt" => "application/vnd.ms-powerpoint",
-        "docx" => "application/msword",
-        "xlsx" => "application/vnd.ms-excel",
-        "pptx" => "application/vnd.ms-powerpoint",
-
-        // open office
-        "odt" => "application/vnd.oasis.opendocument.text",
-        "ods" => "application/vnd.oasis.opendocument.spreadsheet",
-    ];
-
-    if (isset($mimetypes[$ext])) {
-        return $mimetypes[$ext];
-    } else {
-        return "application/octet-stream";
-    }
-}
-
 $uri = parse_url($_SERVER["REQUEST_URI"])["path"];
 
-
 // APIs
+if (str_starts_with($uri, "/api/test")) {
+    echo json_encode(["status" => "success", "message" => "Success from test"]);
+    exit();
+}
 if (str_starts_with($uri, "/api")) {
     echo json_encode(["status" => "success", "message" => "APIs"]);
     exit();
 }
-
-// Assets
-if (str_starts_with($uri, "/assets") || $uri === "/vite.svg") {
-    $path = str_replace("/", DIRECTORY_SEPARATOR, "frontend-web/dist$uri");
-    if (file_exists($path)) {
-        $mime = get_mime_type($path);
-        header("Content-Type: $mime");
-        readfile($path);
-        exit();
-    }
-}
-
-readfile("frontend-web/dist/index.html");
